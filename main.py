613b8d8d53392b3e5874c93e41cfcdaa401d4013
LopDlMa <dulcita11@gmail.com>
2022-03-03 09:36:37 -0600

    Se agregò tortuga que dibuja triàngulo

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import turtle
+
 print("Hola Mundo")
+
+tortuga = turtle.Turtle()
+
+tortuga.left(45)
+tortuga.forward(50)
+tortuga.right(90)
+tortuga.forward(50)
+tortuga.right(135)
+tortuga.forward(75)
+
+turtle.done()
